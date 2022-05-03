fdd542fceb64fc44bcc764d58d9ff3f9d0c6b237
ShidaWei <65539711+weishida01@users.noreply.github.com>
2022-05-03 09:54:08 +0800

    Add files via upload

---
+++ infer.py
@@ -0,0 +1,6 @@
+
+def pp():
+    import os
+    os.system('python D:/daima/13.Paper-Research/2.dataset/3、AwA2/Animals_with_Attributes2/code/AwA2-test-win.py --img_file D:/daima/13.Paper-Research/2.dataset/3、AwA2/Animals_with_Attributes2/code/test_img/lion_10005.jpg --lable lion')
+    return 1
+
